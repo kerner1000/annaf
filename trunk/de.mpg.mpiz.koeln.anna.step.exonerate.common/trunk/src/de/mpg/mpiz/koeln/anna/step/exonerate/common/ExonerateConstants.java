40b40ae353bec840b068552e6e9d7a164cac75cb
2407@gmx.org <2407@gmx.org@6b198794-a421-11de-b950-3d5b5f4ea0aa>
2009-10-01 08:28:27 +0000

    initial import.

---
+++ trunk/de.mpg.mpiz.koeln.anna.step.exonerate.common/trunk/src/de/mpg/mpiz/koeln/anna/step/exonerate/common/ExonerateConstants.java
@@ -0,0 +1,14 @@
+package de.mpg.mpiz.koeln.anna.step.exonerate.common;
+
+public class ExonerateConstants {
+	
+private ExonerateConstants(){}
+	
+	public final static String WORKING_DIR_KEY = "anna.step.exonerate.workingDir";
+	public final static String EXE_DIR_KEY = "anna.step.exonerate.exeDir";
+	public final static String EXE = "exonerate";
+	public static final String EST_FILENAME = "ests.fasta";
+	public static final String INSEQ_FILENAME = "inputsequence.fasta";
+	public static final String RESULT_FILENAME = "exonerate-result.gff";
+
+}
