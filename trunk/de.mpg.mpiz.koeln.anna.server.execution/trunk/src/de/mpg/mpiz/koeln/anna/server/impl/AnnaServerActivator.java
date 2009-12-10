c4a84c9c298373b2fd07858b5307009d7a320dc5
2407@gmx.org <2407@gmx.org@6b198794-a421-11de-b950-3d5b5f4ea0aa>
2009-12-10 11:43:58 +0000

    Unregister service when shutdown.

--- trunk/de.mpg.mpiz.koeln.anna.server.execution/trunk/src/de/mpg/mpiz/koeln/anna/server/impl/AnnaServerActivator.java
+++ trunk/de.mpg.mpiz.koeln.anna.server.execution/trunk/src/de/mpg/mpiz/koeln/anna/server/impl/AnnaServerActivator.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 import de.kerner.commons.logging.Log;
 import de.mpg.mpiz.koeln.anna.server.AnnaServer;
@@ -16,12 +17,14 @@ import de.mpg.mpiz.koeln.anna.server.AnnaServer;
 public class AnnaServerActivator implements BundleActivator {
 
 	private final static Log logger = new Log(AnnaServerActivator.class);
+	private volatile BundleContext context;
 
 	public void start(BundleContext context) throws Exception {
-		final AnnaServer service = new AnnaServerImpl();
+		this.context = context;
+		final AnnaServer service = new AnnaServerImpl(this);
 		context.registerService(AnnaServer.class.getName(), service,
 				new Hashtable<Object, Object>());
-		logger.debug("activated");
+		logger.debug("server started");
 	}
 
 	public void stop(BundleContext context) throws Exception {
@@ -31,5 +34,17 @@ public class AnnaServerActivator implements BundleActivator {
 
 	public String toString() {
 		return this.getClass().getSimpleName();
+	}
+
+	// TODO: this should be obsolete (services do not need to be unregistered in order to shutdown OSGi)
+	void shutdown() {
+		logger.debug("shutting down server");
+		final ServiceReference reference = context.getServiceReference(AnnaServer.class.getName());
+		final boolean b = context.ungetService(reference);
+		if(b){
+			logger.debug("server down");
+		} else {
+			logger.debug("server was not running");
+		}
 	}
 }
